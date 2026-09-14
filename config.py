7cd9d67ab15ca8fc481a0fbc8af52585c9ed66d5
krishnahirwal16-alt <krishnahirwal16@gmail.com>
2026-09-14 22:50:21 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+
+class AppConfig:
+    def __init__(
+        self,
+        port: str,
+        cache_seconds: int,
+        timezone: str,
+        highlightly_api_key: str,
+        highlightly_base_url: str,
+        cricketdata_api_key: str,
+        cricketdata_base_url: str,
+    ):
+        self.port = port
+        self.cache_seconds = cache_seconds
+        self.timezone = timezone
+        self.highlightly_api_key = highlightly_api_key
+        self.highlightly_base_url = highlightly_base_url
+        self.cricketdata_api_key = cricketdata_api_key
+        self.cricketdata_base_url = cricketdata_base_url
+
+    @classmethod
+    def from_env(cls):
+        return cls(
+            port=os.getenv("PORT", "5000"),
+            cache_seconds=int(os.getenv("CRIXDATA_CACHE_SECONDS", "900")),
+            timezone=os.getenv("CRIXDATA_TIMEZONE", "Asia/Kolkata"),
+            highlightly_api_key=os.getenv("HIGHLIGHTLY_API_KEY", ""),
+            highlightly_base_url=os.getenv(
+                "HIGHLIGHTLY_BASE_URL",
+                "https://cricket.highlightly.net",
+            ),
+            cricketdata_api_key=os.getenv("CRICKET_API_KEY", ""),
+            cricketdata_base_url=os.getenv(
+                "CRICKETDATA_BASE_URL",
+                "https://api.cricapi.com/v1",
+            ),
+        )
